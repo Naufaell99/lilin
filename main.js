3ebcbef78771dbe336a06d4ec18f7cec6f7ba231
naufaell <naufallkurnia93@gmail.com>
2025-10-19 04:32:46 +0700

    lilin

---
+++ main.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+  const flame = $("#flame");
+  const txt = $("h1");
+  const backBtn = $("#backBtn");
+  const music = $("#bgMusic")[0];
+
+  // ✅ Putar musik otomatis saat halaman dimuat
+  // (akan berjalan kalau user sudah pernah interaksi di web sebelumnya)
+  music.volume = 0;
+  const startMusic = () => {
+    music.play().then(() => {
+      // fade-in pelan
+      let volume = 0;
+      const fade = setInterval(() => {
+        if (volume < 1) {
+          volume += 0.05;
+          music.volume = volume;
+        } else {
+          clearInterval(fade);
+        }
+      }, 200);
+    }).catch(err => {
+      console.warn("Autoplay musik diblokir browser:", err);
+    });
+  };
+  startMusic();
+
+  // 🔥 Event klik lilin
+  flame.on("click", function () {
+    flame.removeClass("burn").addClass("puff");
+    $(".smoke").each(function () {
+      $(this).addClass("puff-bubble");
+    });
+    $("#glow").remove();
+
+    txt.hide()
+      .html("Hihihihi... Selamat ulang tahun ya sayang 💕 udah berapa kali ucap ini wkwkwk 😚")
+      .delay(750)
+      .fadeIn(500);
+
+    $("#candle").animate({ opacity: ".5" }, 1000);
+
+    // Tampilkan tombol kembali setelah 2 detik
+    setTimeout(() => backBtn.addClass("fade-in"), 2000);
+  });
+});
+
+// 💫 Fungsi berhentiin musik & balik ke halaman awal
+function stopMusicAndBack() {
+  const music = document.getElementById("bgMusic");
+  if (music && !music.paused) {
+    const fadeOut = setInterval(() => {
+      if (music.volume > 0.05) {
+        music.volume -= 0.05;
+      } else {
+        clearInterval(fadeOut);
+        music.pause();
+        music.currentTime = 0;
+        window.location.href = "index.html";
+      }
+    }, 100);
+  } else {
+    window.location.href = "index.html";
+  }
+}
